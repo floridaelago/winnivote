8cb6bba56cf68fe36c38787028f8b40f174fcc2a
Florida Elago <florida.elago@gmail.com>
2014-02-07 13:43:31 -0600

    add idea and brainstorm relation

--- spec/factories/brainstorm_factory.rb
+++ spec/factories/brainstorm_factory.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :brainstorm do
     title       { Faker::Lorem.sentence  }
     description { Faker::Lorem.paragraph }
+
+    factory :brainstorm_with_ideas do
+      ignore do
+        ideas_count 5
+      end
+
+      after(:create) do |brainstorm, evaluator| 
+        create_list(:ideas, evaluator.ideas_count, brainstorm: brainstorm)
+      end
+    end
   end
 end
--- spec/factories/idea_factory.rb
+++ spec/factories/idea_factory.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :idea do
     title       { Faker::Lorem.sentence  }
     description { Faker::Lorem.paragraph }
+    brainstorm
   end
 end
